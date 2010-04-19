eff7490053fa4882f6c689c5d9f04ed928bb4e9a
chen <info@example.com>
2010-04-19 08:56:30 +0000

    - Created stable branch 1.0
    git-svn-id: http://svn.projects.ez.no/ezcomments@298 20a62680-2ad1-4a8a-825d-218b1b6ec9b5

---
+++ stable/1.0/tests/suite.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * File containing ezcomCommentsTestSuite class
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ *
+ */
+class ezcomCommentsTestSuite extends ezpDatabaseTestSuite
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setName( "eZ Comment Test Suite" );
+        $this->addTestSuite( "ezcomCommentTest" );
+        $this->addTestSuite( "ezcomNotificationTest" );
+        $this->addTestSuite( "ezcomSubscriberTest" );
+        $this->addTestSuite( "ezcomSubscriptionTest" );
+        $this->addTestSuite( "ezcomCommentManagerTest" );
+        $this->addTestSuite( "ezcomSubscriptionManagerTest" );
+        $this->addTestSuite( "ezcomNotificationManagerTest" );
+    }
+
+    public static function suite()
+    {
+        return new self();
+    }
+}
+
+?>
